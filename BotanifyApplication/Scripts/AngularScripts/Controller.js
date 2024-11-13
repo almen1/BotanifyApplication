83d71f675f789d92c96471700ba23ddc4a5c089b
almen1 <aeronalmendras@gmail.com>
2024-11-14 00:37:23 +0800

    Added navbar

--- BotanifyApplication/Scripts/AngularScripts/Controller.js
+++ BotanifyApplication/Scripts/AngularScripts/Controller.js
@@ -1,2 +1,15 @@
 ﻿app.controller("BotanifyApplicationController", function ($scope, BotanifyApplicationService) {
+
+    $scope.dropdownVisible = false;
+    $scope.toggleDropdown = function () {
+        $scope.dropdownVisible = !$scope.dropdownVisible;
+    };
+
+    $scope.shopCategories = [
+        { title: 'Shop by collections', links: ['Test Link 1', 'Test Link 2', 'Test Link 3', 'Test Link 4'] },
+        { title: 'Shop by price', links: ['Test Link 1', 'Test Link 2', 'Test Link 3', 'Test Link 4'] },
+        { title: 'Shop by size', links: ['Test Link 1', 'Test Link 2', 'Test Link 3', 'Test Link 4'] },
+        { title: 'Shop by category', links: ['Test Link 1', 'Test Link 2', 'Test Link 3', 'Test Link 4'] }
+    ];
+
 });
